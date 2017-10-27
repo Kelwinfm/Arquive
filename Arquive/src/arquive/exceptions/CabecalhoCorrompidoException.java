f820dc757c5f705a702886f6dcf8c7c0028653bb
sergiodeveloper <sergio-fh@hotmail.com>
2017-10-27 16:46:56 -0200

    Isolando exceções relativas ao processamento do cabeçalho

---
+++ Arquive/src/arquive/exceptions/CabecalhoCorrompidoException.java
@@ -0,0 +1,20 @@
+/**
+ * Arquive
+ *
+ * Equipe desenvolvedora do sistema Arquive para Estrutura de Arquivos
+ * Universidade Estadual de Campinas - 2017
+ */
+package arquive.exceptions;
+
+import java.text.ParseException;
+
+/**
+ * Classe utilizada para geração de exceções na interpretação do cabeçalho
+ */
+public class CabecalhoCorrompidoException extends ParseException {
+
+    public CabecalhoCorrompidoException(String message, int offset) {
+        super("Arquivo corrompido: " + message, offset);
+    }
+
+}
