8fd56cfa50398f088312a00ba95940e24e824c18
sergiodeveloper <sergio-fh@hotmail.com>
2017-11-02 15:18:51 -0200

    Adicionando métodos obterArquivo, apagar arquivo e listar arquivos

--- Arquive/src/archive/controller/ControladorArchive.java
+++ Arquive/src/archive/controller/ControladorArchive.java
@@ -18,6 +18,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFileChooser;
 
 /**
@@ -45,7 +47,7 @@ public class ControladorArchive {
         Archive archive = new Archive(cabecalho, arquivo);
 
         RandomAccessFile acessoArquivo = new RandomAccessFile(arquivo, "rw");
-        CabecalhoDAO.gravarCabecalho(cabecalho, acessoArquivo);
+        CabecalhoDAO.gravarCabecalho(acessoArquivo, cabecalho);
         acessoArquivo.close();
 
         return archive;
@@ -58,8 +60,12 @@ public class ControladorArchive {
 
     public static void fecharSessao() throws IOException {
         archiveAberto = null;
-        acessoArquivoAberto.close();
-        acessoArquivoAberto = null;
+        if (acessoArquivoAberto == null) {
+            assert false : "Nenhum arquivo aberto na sessão";
+        } else {
+            acessoArquivoAberto.close();
+            acessoArquivoAberto = null;
+        }
     }
 
     /**
@@ -71,6 +77,7 @@ public class ControladorArchive {
      */
     public static void inserirArquivo(Arquivo arquivo) throws IOException, CabecalhoEsgotadoException {
         if (archiveAberto == null || acessoArquivoAberto == null) {
+            assert false : "Nenhum arquivo aberto na sessão";
             return;
         }
 
@@ -113,18 +120,102 @@ public class ControladorArchive {
             }
         }
 
-        // Inserir conteúdo binário no archive
-        BinarioDAO.gravarArquivo(archiveAberto, acessoArquivoAberto, arquivo, posicao);
-
-        // Inserir no cabeçalho
-        cabecalho.adicionarItem(new ItemCabecalho(
+        // Novo item para o cabeçalho
+        ItemCabecalho novoItem = new ItemCabecalho(
                 Status.Valido,
                 arquivo.getNome(), posicao, arquivo.getTamanho()
-        ));
+        );
+
+        // Inserir conteúdo binário no archive
+        BinarioDAO.gravarArquivo(archiveAberto, acessoArquivoAberto, arquivo, novoItem);
+
+        // Inserir no cabeçalho
+        cabecalho.adicionarItem(novoItem);
 
         // Gravar cabeçalho no archive
-        CabecalhoDAO.gravarCabecalho(cabecalho, acessoArquivoAberto);
+        CabecalhoDAO.gravarCabecalho(acessoArquivoAberto, cabecalho);
 
+    }
+
+    /**
+     * Obter um arquivo no Archive atualmente aberto na sessão a partir do seu
+     * nome
+     *
+     * @param nome Nome do arquivo a ser localizado no cabeçalho
+     * @throws IOException
+     * @return Arquivo encontrado OU nulo se não for encontrado com este nome
+     */
+    public static Arquivo obterArquivo(String nome) throws IOException {
+        if (archiveAberto == null) {
+            assert false : "Nenhum arquivo aberto na sessão";
+            return null;
+        }
+
+        Cabecalho cabecalho = archiveAberto.getCabecalho();
+
+        for (ItemCabecalho itemCabecalho : cabecalho.getItens()) {
+            if (itemCabecalho.getNome().equals(nome)) {
+
+                Arquivo arquivo = BinarioDAO.lerArquivo(
+                        archiveAberto,
+                        acessoArquivoAberto,
+                        itemCabecalho
+                );
+
+                return arquivo;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Apagar arquivo do Archive atualmente aberto na sessão a partir do seu
+     * nome. Vários serão apagados se tiverem o mesmo nome
+     *
+     * @param nome Nome do arquivo a ser apagado
+     * @throws IOException
+     * @throws archive.exceptions.CabecalhoEsgotadoException
+     */
+    public static void apagarArquivo(String nome)
+            throws IOException, CabecalhoEsgotadoException {
+        if (archiveAberto == null) {
+            assert false : "Nenhum arquivo aberto na sessão";
+            return;
+        }
+
+        Cabecalho cabecalho = archiveAberto.getCabecalho();
+
+        for (ItemCabecalho itemCabecalho : cabecalho.getItens()) {
+            if (itemCabecalho.getNome().equals(nome)) {
+                itemCabecalho.setStatus(Status.Excluido);
+            }
+        }
+
+        // Regravar cabeçalho no arquivo
+        CabecalhoDAO.gravarCabecalho(acessoArquivoAberto, cabecalho);
+    }
+
+    /**
+     * Obter lista de arquivos válidos no archive aberto na sessão
+     *
+     * @return
+     */
+    public static List<ItemCabecalho> listarArquivos() {
+        List<ItemCabecalho> lista = new ArrayList<>();
+
+        if (archiveAberto == null) {
+            assert false : "Nenhum arquivo aberto na sessão";
+            return lista;
+        }
+
+        for (ItemCabecalho itemCabecalho : archiveAberto.getCabecalho().getItens()) {
+            if (itemCabecalho.getStatus() == Status.Valido) {
+                lista.add(itemCabecalho);
+            }
+        }
+
+        return lista;
     }
 
 }
