1fd5b2e7bd869663116068d1354a8e6c4cb7a1f9
sergiodeveloper <sergio-fh@hotmail.com>
2017-11-02 14:47:38 -0200

    Adicionando métodos para ler e escrever arquivos em um Arquive

--- Arquive/src/archive/dao/BinarioDAO.java
+++ Arquive/src/archive/dao/BinarioDAO.java
@@ -7,16 +7,32 @@
 package archive.dao;
 
 import archive.model.Archive;
+import archive.model.Arquivo;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * Responsável por carregar e atualizar o conteúdo binário de um archive
  */
 public class BinarioDAO extends AbstractDAO {
 
-    public static byte[] lerArquivo(Archive arquive, int posicao, int tamanho) {
+    public static void gravarArquivo(
+            Archive archive, RandomAccessFile acessoArchive,
+            Arquivo arquivo,
+            int posicao
+    ) throws IOException {
+        acessoArchive.seek(posicao);
+        acessoArchive.write(arquivo.getConteudo());
+    }
 
-        // TODO
-        return null;
+    public static Arquivo lerArquivo(
+            Archive archive, RandomAccessFile acessoArchive,
+            String nome, int posicao, int tamanho
+    ) throws IOException {
+        acessoArchive.seek(posicao);
+        byte[] bytes = lerBytes(acessoArchive, posicao, tamanho);
+
+        return new Arquivo(nome, bytes);
     }
 
 }
