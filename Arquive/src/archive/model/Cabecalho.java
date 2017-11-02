f385314e4f24908465da797f4f6ebf6b9c8d5e82
sergiodeveloper <sergio-fh@hotmail.com>
2017-11-02 14:49:02 -0200

    Adicionando métodos adicionar item e obter o item do último arquivo

--- Arquive/src/archive/model/Cabecalho.java
+++ Arquive/src/archive/model/Cabecalho.java
@@ -26,8 +26,36 @@ public class Cabecalho {
         itens.add(item);
     }
 
+    public void removerItem(ItemCabecalho item) {
+        itens.remove(item);
+    }
+
     public List<ItemCabecalho> getItens() {
         return itens;
+    }
+
+    /**
+     * Encontrar último arquivo do cabeçalho (com maior valor posicao)
+     *
+     * @return Item se referindo ao último arquivo dentro do archive OU null se
+     * não houver arquivo
+     */
+    public ItemCabecalho getItemUltimoArquivo() {
+        if (itens.isEmpty()) {
+            return null;
+        }
+
+        ItemCabecalho selecionado = itens.get(0);
+        int maiorPosicao = selecionado.getPosicao();
+
+        for (ItemCabecalho item : itens) {
+            if (item.getPosicao() > maiorPosicao) {
+                selecionado = item;
+                maiorPosicao = selecionado.getPosicao();
+            }
+        }
+
+        return selecionado;
     }
 
 }
