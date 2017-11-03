e3ac60b31379bbe6c1569ca59a1b0090b9d39a42
sergiodeveloper <sergio-fh@hotmail.com>
2017-11-02 22:55:23 -0200

    Corrigindo interpretador do cabeçalho

--- Arquive/src/archive/controller/InterpretadorCabecalho.java
+++ Arquive/src/archive/controller/InterpretadorCabecalho.java
@@ -98,7 +98,8 @@ public class InterpretadorCabecalho {
         Cabecalho cabecalho = new Cabecalho();
 
         // Interpretar itens do cabeçalho
-        for (posicaoAtual = 0; posicaoAtual < bytes.length; posicaoAtual++) {
+        posicaoAtual = 0;
+        while (posicaoAtual < bytes.length) {
             if (bytes[posicaoAtual] == 0) {
                 // Fim do cabeçalho
                 break;
